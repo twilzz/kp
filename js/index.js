14a6cfef10ea4eacc780fb7532252a8e3e044526
Alexey aka Twils <twils@ya.ru>
2021-03-15 22:34:09 +0300

    время дизайнится

--- js/index.js
+++ js/index.js
@@ -1,15 +1,13 @@
 document.addEventListener("DOMContentLoaded", () => {
+  //БЛОК ПОЗИЦИЙ ДЛЯ РАСЧЕТА И РАБОТЫ С БД
+{
   const resultDiv = document.querySelector(".result_grid_body"),
-    buttonPlace = document.querySelector(".button_line"),
-    divExist = document.querySelector('.exist');
-  let subButton, buttonReset;
+        buttonAdd = document.querySelector("#add2base"),
+        buttonShow = document.querySelector("#showbase"),
+        popupWindow = document.querySelector(".popup__window");
+  let subButton;
   let prices = [];
-
-  const buttonAdd = document.querySelector("#add2base"),
-    buttonShow = document.querySelector("#showbase"),
-    buttonStart = document.querySelector("#startCalc"),
-    popupWindow = document.querySelector(".popup__window");
-
+showButton();
   buttonAdd.addEventListener("click", () => {
     generateWindow();
     popupWindow.innerHTML = `<div class="form">
@@ -27,25 +25,21 @@ document.addEventListener("DOMContentLoaded", () => {
                                   <input type="submit" value="Добавить">
                                 </form>
                               </div>`;
-
     const form = document.querySelector("form");
     postData(form);
   });
   buttonShow.addEventListener("click", () => {
     generateWindow();
     let newGridTop = document.createElement("div");
-    newGridTop.classList.add("result_grid_top");
+    newGridTop.classList.add("result_grid_top_window");
     newGridTop.innerHTML = `<div class='result_grid_position'>ID</div>
-                          <div class='result_grid_position'>Тип</div>
-                          <div class='result_grid_position'>Описание позиции</div>
-                          <div class='result_grid_position'>Коэфф</div>
-                          <div class='result_grid_position'>СЕБЕСТ ед.</div>
+                            <div class='result_grid_position'>Тип</div>
+                            <div class='result_grid_position'>Описание позиции</div>
+                            <div class='result_grid_position'>Коэфф</div>
+                            <div class='result_grid_position'>СЕБЕСТ ед.</div>
                           `;
     popupWindow.append(newGridTop);
     getData();
-  });
-  buttonStart.addEventListener("click", () => {
-    generateWindow();
   });
   function generateWindow() {
     popupWindow.classList.toggle("popup__window_show");
@@ -53,8 +47,6 @@ document.addEventListener("DOMContentLoaded", () => {
       popupWindow.removeChild(popupWindow.firstChild);
     }
   }
-
-  //ШАГ 1 - СБОР РАСЧЕТНОГО ЛИСТА
   function postData(form) {
     form.addEventListener("submit", (e) => {
       e.preventDefault();
@@ -63,7 +55,6 @@ document.addEventListener("DOMContentLoaded", () => {
       axios.post("http://localhost:3000/products", postObject);
     });
   }
-
   async function getData() {
     await axios.get("http://localhost:3000/products").then((data) => {
       data.data.forEach((item) => {
@@ -71,8 +62,8 @@ document.addEventListener("DOMContentLoaded", () => {
       });
       subButton = document.createElement("button");
       subButton.innerText = "Добавить в расчет";
-      subButton.style.width ='150px';
-      subButton.style.marginTop ='5px';
+      subButton.style.width = "150px";
+      subButton.style.marginTop = "5px";
       popupWindow.append(subButton);
       const rowOfProduct = document.querySelectorAll(".result_grid");
       rowOfProduct.forEach((item, index) =>
@@ -86,7 +77,6 @@ document.addEventListener("DOMContentLoaded", () => {
       });
     });
   }
-
   function renderExists({ id, type, descripton, koeff, price }) {
     const newRow = document.createElement("div");
     newRow.classList.add("result_grid");
@@ -97,62 +87,44 @@ document.addEventListener("DOMContentLoaded", () => {
                         <div class='result_grid_position' name="price">${price}</div>`;
     popupWindow.append(newRow);
   }
- 
   function copyRows(selector) {
     const rows = document.querySelectorAll(selector);
-    rows.forEach((item, index) => {
+    rows.forEach((item) => {
       if (item.classList.contains("result_grid_clicked")) {
-        divExist.append(item);
-        // item.classList.remove("result_grid_clicked");
-      } else {
-        item.remove();
-      }
-    });
+        let divs4remove = item.querySelectorAll("div");
+        divs4remove.forEach((div) => {
+          if (
+            div.getAttribute("name") == "koeff" ||
+            div.getAttribute("name") == "price"
+          ) {
+            div.remove();
+          }
+        });
+        const newPriceDiv = document.createElement("div"),
+              quantityDiv = document.createElement("div"),
+              totalDiv = document.createElement("div");
 
-  }
+        newPriceDiv.classList.add("result_grid_position");
+        newPriceDiv.style.cssText = 'border-left: 1px solid blue; border-right: 1px solid blue;';
+        quantityDiv.classList.add("result_grid_position");
+        quantityDiv.style.borderRight = '1px solid blue';
+        totalDiv.classList.add("result_grid_position");
 
-  //ШАГ 2 - РАБОТА С ОБЪЕМАМИ
-  function recreateNewData() {
-    const gridTopParent = document.querySelector(".result_grid_top");
-    gridTopParent.remove();
-    let newGridTop = document.createElement("div");
-    newGridTop.classList.add("result_grid_top");
-    newGridTop.style.gridTemplateColumns = "0.5fr 3fr 0.5fr 0.5fr 0.5fr";
-    newGridTop.innerHTML = `<div class='result_grid_position'>ID</div>
-                          <div class='result_grid_position'>ОПИСАНИЕ</div>
-                          <div class='result_grid_position'>ЦЕНА ЗА ЕД</div>
-                          <div class='result_grid_position'>КОЛИЧЕСТВО</div>
-                          <div class='result_grid_position'>ЦЕНА ОБЪЕМА</div>
-                          `;
-    resultDiv.before(newGridTop);
-    let divs = document.querySelectorAll(".result_grid_position");
-    divs.forEach((item) => {
-      if (
-        item.getAttribute("name") == "type" ||
-        item.getAttribute("name") == "koeff"
-      ) {
-        item.remove();
-      }
-      if (item.getAttribute("name") == "price") {
-        item.textContent =
-          item.previousElementSibling.getAttribute(`data-koeff`) *
-          item.previousElementSibling.getAttribute(`data-price`);
+        newPriceDiv.textContent = `${divs4remove[2].getAttribute('data-koeff')*divs4remove[2].getAttribute('data-price')}`;
+        quantityDiv.setAttribute("name", "quantity");
+        quantityDiv.innerHTML = `<input type="text" placeholder="Кол-во">`;
+        totalDiv.innerHTML = `<span style="text-align: center">0</span>руб`;
+
+        item.appendChild(newPriceDiv);
+        item.appendChild(quantityDiv);
+        item.appendChild(totalDiv);
+        
+        item.style.gridTemplateColumns = '0.5fr 0.5fr 3fr 0.5fr 1fr 0.5fr';
+        item.classList.remove('result_grid_clicked');
+        resultDiv.append(item);
       }
     });
-    let divsParent = document.querySelectorAll(".result_grid");
-    divsParent.forEach((item) => {
-      item.style.gridTemplateColumns = "0.5fr 3fr 0.5fr 0.5fr 0.5fr";
-      let quantityDiv = document.createElement("div");
-      quantityDiv.classList.add("result_grid_position");
-      quantityDiv.setAttribute("name", "quantity");
-      quantityDiv.innerHTML = `<input type="text" placeholder="Кол-во">`;
-      item.appendChild(quantityDiv);
-      let totalDiv = document.createElement("div");
-      totalDiv.classList.add("result_grid_position");
-      totalDiv.innerHTML = `<span style="text-align: center">0</span>руб`;
-      item.appendChild(totalDiv);
-    });
-    recreateButtons("К расчету", reducePrices);
+    showButton();
     startCalculating();
   }
   function startCalculating() {
@@ -161,17 +133,68 @@ document.addEventListener("DOMContentLoaded", () => {
     );
     quantityInputs.forEach((item) => {
       item.addEventListener("input", () => {
-        item.parentElement.nextSibling.innerHTML = `<span>${
-          item.value * +item.parentElement.previousSibling.textContent
-        }</span>руб.`;
+        item.parentElement.nextSibling.innerHTML = `<span>${(item.value * +item.parentElement.previousSibling.textContent).toLocaleString()
+        }</span> руб.`;
       });
     });
   }
-
   function reducePrices() {
+    prices = [];
     let spanPrices = document.querySelectorAll("span");
-    spanPrices.forEach((item) => prices.push(+item.textContent));
+    spanPrices.forEach((item) => {
+      let newNum = item.textContent.replace(/\s/g,'');
+      newNum = newNum.replace(/,/g,'.');
+      prices.push(+newNum);
+    });
+    document.querySelector('.total_result div').textContent = `${(prices.reduce((a, b) => a + b)).toLocaleString()} руб`;
     console.log(prices.reduce((a, b) => a + b));
   }
+  function showButton(){
+    if (resultDiv.querySelector('div')) {
+      console.log('not empty', resultDiv.querySelector('div'));
+      document.querySelector('.button_line').style.display = 'grid';
+      document.querySelector('.total_result').style.display = 'grid';
+      document.querySelector('.result_grid_top').style.display = 'grid';
+      const calcbutton = document.querySelector('.button_line button');
+      calcbutton.addEventListener('click', ()=> {
+        reducePrices();
+      });
+    } else {
+      console.log('empty');
+    }
+  }
+}
+  // БЛОК СТАРТОВЫХ ЗНАЧЕНИЙ
+const saveBtn = document.querySelector('#saveBtn'), 
+      editBtn = document.querySelector('#editBtn'),
+      dataLables = document.querySelectorAll('.headers li'),
+      dataInsteadInputs = document.querySelectorAll('.values li');
+    
+saveBtn.addEventListener('click', ()=>{
+  saveData();
+});
+editBtn.addEventListener('click', ()=>{
+  editData();
+});
+function saveData (){
+  dataInputs = document.querySelectorAll('.values input');
+  dataInputs.forEach((input,index) => {
+    if (!input.value) {
+      input.style.borderColor = 'red';
+      input.value = "Заполнить или 0";
+      console.log(input.style);
+    } else {
+      saveBtn.disabled = true;
+      dataInsteadInputs[index].innerHTML = `<span>${input.value}</span>`;
+    }
+})}
+function editData(){
+  const spans2Inputs = document.querySelectorAll('.values span'),
+  li = document.querySelectorAll('.values li');
+  li.forEach((inp, index) => {
+  inp.innerHTML = `<input type="text" value='${spans2Inputs[index].textContent}'>`;
+});
+saveBtn.disabled = false;
+}
 
 });
